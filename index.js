a9fe218b9cc223e0bdeddbdbcdaf37c3da061ac3
Rafael <rcfansub@gmail.com>
2022-12-25 00:49:42 -0300

    0.1.200

--- index.js
+++ index.js
@@ -21,25 +21,6 @@ function openTab(evt, cityName) {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
-var checkboxValues = JSON.parse(localStorage.getItem('checkboxValues')) || {},
-    $checkboxes = $("#checkbox-container :checkbox");
-
-$checkboxes.on("change", function(){
-  $checkboxes.each(function(){
-    checkboxValues[this.id] = this.checked;
-  });
-  
-  localStorage.setItem("checkboxValues", JSON.stringify(checkboxValues));
-});
-
-// On page load
-$.each(checkboxValues, function(key, value) {
-  $("#" + key).prop('checked', value);
-});
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////
-
 function deleteAllCookies() {
     var cookies = document.cookie.split(";");
 
@@ -52,6 +33,7 @@ function deleteAllCookies() {
 }
 
 function clearAndRedirect(link) {
+    localStorage.clear()
     deleteAllCookies();
     document.location = link;
 }
